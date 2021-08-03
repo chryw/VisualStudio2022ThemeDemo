07a483069a570f1158c5f76c42b90490ffadca08
Cherry Wang 🍒(DevDiv) <cherwan@microsoft.com>
2021-08-03 12:45:47 -0700

    Added some unit tests to demo the test explorer and LUT area

---
+++ NewDarkThemeDemoWebAppTests/StartupTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewDarkThemeDemoWebApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewDarkThemeDemoWebApp.Tests
+{
+    [TestClass()]
+    public class StartupTests
+    {
+        [TestMethod()]
+        public void Fail01()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Fail02()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Fail03()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException01()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException02()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException021()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException022()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException03()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void Pass01()
+        {
+            Console.WriteLine("First one passed!");
+        }
+
+        [TestMethod()]
+        public void Pass02()
+        {
+            Console.WriteLine("Second one passed too!");
+        }
+
+        [TestMethod()]
+        public void Pass31()
+        {
+            Console.WriteLine("Third one also passed!");
+        }
+    }
+}
