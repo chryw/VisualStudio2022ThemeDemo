7f704c959ecccf162539ea006bd824360b713df2
Cherry Wang 🍒(DevDiv) <cherwan@microsoft.com>
2021-08-03 13:29:27 -0700

    Remove error for main branch

--- NewDarkThemeDemoWebApp/Pages/Error.cshtml.cs
+++ NewDarkThemeDemoWebApp/Pages/Error.cshtml.cs
@@ -1,12 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Threading.Tasks;
 
 namespace NewDarkThemeDemoWebApp.Pages
 {
@@ -27,11 +22,6 @@ namespace NewDarkThemeDemoWebApp.Pages
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-            Console.WriteLine("Hello! This is a test message.");
-            
-            Console.WriteLine(Hello! This is a test message);
-            return;
-            Console.WriteLine("Hello! This is another test message.");
         }
     }
 }
