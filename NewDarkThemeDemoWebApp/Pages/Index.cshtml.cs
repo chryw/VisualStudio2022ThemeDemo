b2260a2ce19c703aceb58dc8a28902072bf3c22d
Cherry Wang 🍒(DevDiv) <cherwan@microsoft.com>
2021-08-03 14:22:36 -0700

    Added a timer to demo debug

--- NewDarkThemeDemoWebApp/Pages/Index.cshtml.cs
+++ NewDarkThemeDemoWebApp/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NewDarkThemeDemoWebApp.Pages
@@ -19,7 +20,55 @@ namespace NewDarkThemeDemoWebApp.Pages
 
         public void OnGet()
         {
-             
+            // Create an AutoResetEvent to signal the timeout threshold in the
+            // timer callback has been reached.
+            var autoEvent = new AutoResetEvent(false);
+
+            var statusChecker = new StatusChecker(10);
+
+            // Create a timer that invokes CheckStatus after one second, 
+            // and every 1/4 second thereafter.
+            Console.WriteLine("{0:h:mm:ss.fff} Creating timer.\n",
+                              DateTime.Now);
+            var stateTimer = new Timer(statusChecker.CheckStatus,
+                                       autoEvent, 1000, 250);
+
+            // When autoEvent signals, change the period to every half second.
+            autoEvent.WaitOne();
+            stateTimer.Change(0, 500);
+            Console.WriteLine("\nChanging period to .5 seconds.\n");
+
+            // When autoEvent signals the second time, dispose of the timer.
+            autoEvent.WaitOne();
+            stateTimer.Dispose();
+            Console.WriteLine("\nDestroying timer.");
+        }
+    }
+    public class StatusChecker
+    {
+        private int invokeCount;
+        private int maxCount;
+
+        public StatusChecker(int count)
+        {
+            invokeCount = 0;
+            maxCount = count;
+        }
+
+        // This method is called by the timer delegate.
+        public void CheckStatus(Object stateInfo)
+        {
+            AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
+            Console.WriteLine("{0} Checking status {1,2}.",
+                DateTime.Now.ToString("h:mm:ss.fff"),
+                (++invokeCount).ToString());
+
+            if (invokeCount == maxCount)
+            {
+                // Reset the counter and signal the waiting thread.
+                invokeCount = 0;
+                autoEvent.Set();
+            }
         }
     }
 }
