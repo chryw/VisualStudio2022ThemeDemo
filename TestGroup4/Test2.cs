f684357fadb3763a36242d8103ef65813c2ba021
Cherry Wang (DevDiv) <cherwan@microsoft.com>
2022-02-25 18:11:17 -0800

    Added more projects to test colorized tabs

---
+++ TestGroup4/Test2.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestGroup4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGroup4
+{
+    [TestClass()]
+    public class Test2
+    {
+        [TestMethod()]
+        public void Fail01()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Fail02()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Fail03()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException01()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException02()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException021()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException022()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void NotImplementedException03()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void Pass01()
+        {
+            Console.WriteLine("First one passed!");
+        }
+
+        [TestMethod()]
+        public void Pass02()
+        {
+            Console.WriteLine("Second one passed too!");
+        }
+
+        [TestMethod()]
+        public void Pass31()
+        {
+            Console.WriteLine("Third one also passed!");
+        }
+    }
+}
